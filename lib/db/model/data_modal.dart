3f2a7d97e5c3200a06a508987702ba57ded4f447
AbdulKhadhar <abdulkhadhar13@gmail.com>
2022-06-13 05:18:21 +0530

    db

--- lib/db/model/data_modal.dart
+++ lib/db/model/data_modal.dart
@@ -2,5 +2,5 @@ class StudentModel {
   final String name;
   final String age;
 
-  StudentModel({required this.name, required this.age});
+  StudentModel({required this.name,  required this.age});
 }
